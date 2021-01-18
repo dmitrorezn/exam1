29c34b7e0e03b53eedad387006019d3f30c02a37
evgeniy132 <evgeniy132@gmail.com>
2021-01-18 12:36:50 +0200

    done

---
+++ LIFO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exam
+{
+    public class LIFO
+    {
+        private int[] _array;
+        private int _length;
+
+
+        public LIFO(int size)
+        {
+            _array = new int[size];
+        }
+
+
+
+        public int Length
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+
+        public int Count()
+        {
+            return _length;
+        }
+        public int Get()
+        {
+            lock (_array)
+            {
+                var index = _array.Length - _length;
+                var item = _array[index];
+                _array[index] = 0;
+                _length--;
+                return item;
+            }
+        }
+
+        public void Push(int item)
+        {
+            lock (_array)
+            {
+                if (_length == _array.Length)
+                    return;
+                _array[_array.Length - _length - 1] = item;
+                _length++;
+            }
+        }
+    }
+}
---
+++ examLIFO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace examLIFO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new LIFO(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() =>
+                {
+                    PushAndGet(stack);
+                });
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine(stack.Count());
+
+            Console.ReadLine();
+        }
+        private static void PushAndGet(LIFO stack)
+        {
+            for (int i = 0; i < stack.Length; i++)
+            {
+                stack.Push(i);
+                stack.Get();
+                Console.WriteLine("pushed got");
+            }
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
